fbe102ea20fde20738b2a24b9781429264efc55a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-11-30 16:06:33 +0100

    Add server_env_connector_jira

---
+++ server_env_connector_jira/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{'name': 'Server environment for JIRA Connector',
+ 'version': '9.0.1.0.0',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'license': 'AGPL-3',
+ 'category': 'Tools',
+ 'depends': ['server_environment',  # oca/server-tools
+             'connector_jira',
+             ],
+ 'website': 'http://www.camptocamp.com',
+ 'data': [],
+ 'test': [],
+ 'installable': True,
+ }
