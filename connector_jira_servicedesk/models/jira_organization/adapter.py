a24998b26e2b0380afd964c196de777cf85c6f8f
Patrick Tombez <patrick.tombez@camptocamp.com>
2019-01-29 15:50:25 +0100

    Add connector_jira_servicedesk
    Map projects by external_id + set of jira orgs
    
    Project bindings now can be assigned to one or more jira organizations.
    The binding for the project accept an additional argument for
    organizations. A task will be linked with the project having the exact
    same set of organizations that it has, or fallback to a project without
    organization.
    
    A constraint ensures that you cannot have several projects with the same
    set of organizations or 2 projects without organization.
    
    The link wizard has a new step to select the organization.
    
    The REST API for Serviced Desk is a different one.
    
    The former code was based on https://github.com/pycontribs/jira/pull/388
    which is closed and unmaintained. We only need to read the organizations
    from the servicedesk REST API and the local code is minimal.
    
    We can now use the normal jira library.

---
+++ connector_jira_servicedesk/models/jira_organization/adapter.py
@@ -0,0 +1,61 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+import jira
+from jira.utils import CaseInsensitiveDict
+
+from odoo.addons.component.core import Component
+
+
+class Organization(jira.resources.Resource):
+    """A Service Desk Organization."""
+
+    def __init__(self, options, session, raw=None):
+        super().__init__(
+            'organization/{0}',
+            options,
+            session,
+            '{server}/rest/servicedeskapi/{path}'
+        )
+        if raw:
+            self._parse_raw(raw)
+
+
+class OrganizationAdapter(Component):
+    _name = 'jira.organization.adapter'
+    _inherit = ['jira.webservice.adapter']
+    _apply_on = ['jira.organization']
+
+    # The Service Desk REST API returns an error if this header
+    # is not used. The API may change so they want an agreement for
+    # the client about this.
+    _desk_headers = CaseInsensitiveDict({'X-ExperimentalApi': 'opt-in'})
+
+    def __init__(self, work_context):
+        super().__init__(work_context)
+        self.client._session.headers.update(self._desk_headers)
+
+    def read(self, id_):
+        organization = Organization(
+            self.client._options,
+            self.client._session
+        )
+        organization.find(id_)
+        return organization.raw
+
+    def search(self):
+        base = (self.client._options['server'] +
+                '/rest/servicedeskapi/organization')
+        # By default, a GET on the REST API returns only one page with the
+        # first 50 rows. Here, client is an instance of the jira library's JIRA
+        # class, which provides a _fetch_pages method to fetch pages.
+        # maxResults=False means it will try to get all pages.
+        orgs = self.client._fetch_pages(
+            Organization,
+            'values',
+            'organization',
+            # limit to False will get them in batch
+            maxResults=False,
+            base=base
+        )
+        return [org.id for org in orgs]
