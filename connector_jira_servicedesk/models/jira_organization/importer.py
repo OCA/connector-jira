c5bdf62ff821ba1caed5750eb411549dfb754fdd
Patrick Tombez <patrick.tombez@camptocamp.com>
2019-01-29 15:50:25 +0100

    Add connector_jira_servicedesk
    Map projects by external_id + set of jira orgs
    
    Project bindings now can be assigned to one or more jira organizations.
    The binding for the project accept an additional argument for
    organizations. A task will be linked with the project having the exact
    same set of organizations that it has, or fallback to a project without
    organization.
    
    A constraint ensures that you cannot have several projects with the same
    set of organizations or 2 projects without organization.
    
    The link wizard has a new step to select the organization.
    
    The REST API for Serviced Desk is a different one.
    
    The former code was based on https://github.com/pycontribs/jira/pull/388
    which is closed and unmaintained. We only need to read the organizations
    from the servicedesk REST API and the local code is minimal.
    
    We can now use the normal jira library.

---
+++ connector_jira_servicedesk/models/jira_organization/importer.py
@@ -0,0 +1,36 @@
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo.addons.connector.components.mapper import mapping
+
+from odoo.addons.component.core import Component
+
+
+class OrganizationMapper(Component):
+    _name = 'jira.organization.mapper'
+    _inherit = ['base.import.mapper']
+    _apply_on = 'jira.organization'
+
+    direct = [
+        ('name', 'name'),
+    ]
+
+    @mapping
+    def backend_id(self, record):
+        return {'backend_id': self.backend_record.id}
+
+
+class OrganizationBatchImporter(Component):
+    """ Import the Jira Organizations
+
+    For every id in in the list of organizations, a direct import is done.
+    """
+    _name = 'jira.organization.batch.importer'
+    _inherit = 'jira.direct.batch.importer'
+    _apply_on = ['jira.organization']
+
+    def run(self):
+        """ Run the synchronization """
+        record_ids = self.backend_adapter.search()
+        for record_id in record_ids:
+            self._import_record(record_id)
