c4768e27f76e1a2b2306eeb9d79f76ac95805827
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-01-30 12:51:34 +0100

    Replace text_field by options
    text_field as attribute could not by validated by RNG, options is
    always valid
    
    Add base to support both export and link with projects
    
    Now linking a project with JIRA must be done through a wizard instead
    of the binding view. It must be either export (create the project on
    jira) or link (link with an existing project on jira).
    
    We keep this choice in the binding so we never export data to JIRA
    when we chose the link only.

--- connector_jira/__manifest__.py
+++ connector_jira/__manifest__.py
@@ -29,6 +29,7 @@
  'data': [
      'views/jira_menus.xml',
      'wizards/jira_backend_auth_views.xml',
+     'views/project_link_jira_views.xml',
      'views/jira_backend_views.xml',
      'views/jira_backend_report_templates.xml',
      'views/project_project_views.xml',
