933681da4ecc782c69c2d03ad5fd8b94d128a73d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-01-31 08:24:34 +0100

    Migrate server_env_connector_jira

--- server_env_connector_jira/__manifest__.py
+++ server_env_connector_jira/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {'name': 'Server environment for JIRA Connector',
- 'version': '9.0.1.0.0',
+ 'version': '11.0.1.0.0',
  'author': 'Camptocamp,Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Tools',
