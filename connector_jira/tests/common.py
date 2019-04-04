3b562c6f7417a0300085a6645f6cccb8190ad032
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-04-04 13:43:32 +0200

    Add first test for analytic account lines

--- connector_jira/tests/common.py
+++ connector_jira/tests/common.py
@@ -55,29 +55,30 @@ or to record the test again (in such case, IDs may change).
 
 """
 
+import os
+import logging
+import pprint
+
+from os.path import dirname, join
 
 from odoo.addons.component.tests.common import SavepointComponentCase
 
-from os.path import dirname, join
-import os
-
 from vcr import VCR
-import logging
 
 _logger = logging.getLogger(__name__)
 
-jira_test_url = os.environ.get('JIRA_TEST_URL', 'http://jira:8080')
-jira_test_token_access = os.environ.get('JIRA_TEST_TOKEN_ACCESS', '')
-jira_test_token_secret = os.environ.get('JIRA_TEST_TOKEN_SECRET', '')
+jira_test_url = os.environ.get("JIRA_TEST_URL", "http://jira:8080")
+jira_test_token_access = os.environ.get("JIRA_TEST_TOKEN_ACCESS", "")
+jira_test_token_secret = os.environ.get("JIRA_TEST_TOKEN_SECRET", "")
 
 
 def get_recorder(**kw):
     defaults = dict(
-        record_mode='once',
-        cassette_library_dir=join(dirname(__file__), 'fixtures/cassettes'),
-        path_transformer=VCR.ensure_suffix('.yaml'),
-        match_on=['method', 'path', 'query'],
-        filter_headers=['Authorization'],
+        record_mode="once",
+        cassette_library_dir=join(dirname(__file__), "fixtures/cassettes"),
+        path_transformer=VCR.ensure_suffix(".yaml"),
+        match_on=["method", "path", "query"],
+        filter_headers=["Authorization"],
         decode_compressed_response=True,
     )
     defaults.update(kw)
@@ -93,48 +94,175 @@ class JiraTransactionCase(SavepointComponentCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.backend_record = cls.env.ref(
-            'connector_jira.jira_backend_demo'
+        cls.backend_record = cls.env.ref("connector_jira.jira_backend_demo")
+        cls.backend_record.write(
+            {
+                "uri": jira_test_url,
+                "access_token": jira_test_token_access,
+                "access_secret": jira_test_token_secret,
+                "epic_link_field_name": "customfield_10101",
+            }
         )
-        cls.backend_record.write({
-            'uri': jira_test_url,
-            'access_token': jira_test_token_access,
-            'access_secret': jira_test_token_secret,
-            'epic_link_field_name': 'customfield_10101',
-        })
 
     _base_issue_types = [
-        ('Task', '10002'),
-        ('Sub-task', '10003'),
-        ('Story', '10001'),
-        ('Bug', '10004'),
-        ('Epic', '10000'),
+        ("Task", "10002"),
+        ("Sub-task", "10003"),
+        ("Story", "10001"),
+        ("Bug", "10004"),
+        ("Epic", "10000"),
     ]
+
+    @classmethod
+    def _link_user(cls, user, jira_login):
+        return (
+            cls.env["jira.res.users"]
+            .with_context(no_connector_export=True)
+            .create(
+                {
+                    "odoo_id": user.id,
+                    "backend_id": cls.backend_record.id,
+                    "external_id": jira_login,
+                }
+            )
+        )
 
     @classmethod
     def _create_issue_type_bindings(cls):
         for name, jira_id in cls._base_issue_types:
-            cls.env['jira.issue.type'].create({
-                'name': name,
-                'backend_id': cls.backend_record.id,
-                'external_id': jira_id,
-            })
+            cls.env["jira.issue.type"].create(
+                {
+                    "name": name,
+                    "backend_id": cls.backend_record.id,
+                    "external_id": jira_id,
+                }
+            )
 
-    def _create_project_binding(self, project, sync_action='link',
-                                issue_types=None, **extra):
+    @classmethod
+    def _create_project_binding(
+        cls, project, sync_action="link", issue_types=None, **extra
+    ):
         values = {
-            'odoo_id': project.id,
-            'jira_key': 'TEST',
-            'sync_action': sync_action,
-            'backend_id': self.backend_record.id,
+            "odoo_id": project.id,
+            "jira_key": "TEST",
+            "sync_action": sync_action,
+            "backend_id": cls.backend_record.id,
             # dummy id
-            'external_id': '9999',
+            "external_id": "9999",
         }
         if issue_types:
-            values.update({
-                'sync_issue_type_ids': [(6, 0, issue_types.ids)],
-            })
+            values.update({"sync_issue_type_ids": [(6, 0, issue_types.ids)]})
         values.update(**extra)
-        return self.env['jira.project.project'].with_context(
-            no_connector_export=True
-        ).create(values)
+        return (
+            cls.env["jira.project.project"]
+            .with_context(no_connector_export=True)
+            .create(values)
+        )
+
+    @classmethod
+    def _create_task_binding(cls, task, **extra):
+        values = {
+            "odoo_id": task.id,
+            "jira_key": "TEST",
+            "backend_id": cls.backend_record.id,
+            # dummy id
+            "external_id": "9999",
+        }
+        values.update(**extra)
+        return (
+            cls.env["jira.project.task"]
+            .with_context(no_connector_export=True)
+            .create(values)
+        )
+
+    # copy of 12.0's method, with 'black' applied
+    # https://github.com/odoo/odoo/blob/251021796c25676f5c341e0a3fc3ba4c9feb85bb/odoo/tests/common.py#L294
+    # to remove when migrating to 12.0
+    def assertRecordValues(self, records, expected_values):
+        """ Compare a recordset with a list of dictionaries representing the
+        expected results.
+        This method performs a comparison element by element based on their
+        index. Then, the order of the expected values is extremely important.
+        Note that:
+          - Comparison between falsy values is supported: False match with
+            None.
+          - Comparison between monetary field is also treated according the
+            currency's rounding.
+          - Comparison between x2many field is done by ids. Then, empty
+            expected ids must be [].
+          - Comparison between many2one field id done by id. Empty comparison
+            can be done using any falsy value.
+        :param records:               The records to compare.
+        :param expected_values: List of dicts expected to be exactly matched in
+        records
+        """
+
+        def _compare_candidate(record, candidate):
+            """ Return True if the candidate matches the given record """
+            for field_name in candidate.keys():
+                record_value = record[field_name]
+                candidate_value = candidate[field_name]
+                field_type = record._fields[field_name].type
+                if field_type == "monetary":
+                    # Compare monetary field.
+                    currency_field_name = record._fields[
+                        field_name
+                    ].currency_field
+                    record_currency = record[currency_field_name]
+                    if (
+                        record_currency.compare_amounts(
+                            candidate_value, record_value
+                        )
+                        if record_currency
+                        else candidate_value != record_value
+                    ):
+                        return False
+                elif field_type in ("one2many", "many2many"):
+                    # Compare x2many relational fields.
+                    # Empty comparison must be an empty list to be True.
+                    if set(record_value.ids) != set(candidate_value):
+                        return False
+                elif field_type == "many2one":
+                    # Compare many2one relational fields.
+                    # Every falsy value is allowed to compare with an empty
+                    # record.
+                    if (
+                        record_value or candidate_value
+                    ) and record_value.id != candidate_value:
+                        return False
+                elif (
+                    candidate_value or record_value
+                ) and record_value != candidate_value:
+                    # Compare others fields if not both interpreted as falsy
+                    # values.
+                    return False
+            return True
+
+        def _format_message(records, expected_values):
+            """ Return a formatted representation of records/expected_values.
+            """
+            all_records_values = records.read(
+                list(expected_values[0].keys()), load=False
+            )
+            msg1 = "\n".join(pprint.pformat(dic) for dic in all_records_values)
+            msg2 = "\n".join(pprint.pformat(dic) for dic in expected_values)
+            return "Current values:\n\n%s\n\nExpected values:\n\n%s" % (
+                msg1,
+                msg2,
+            )
+
+        # if the length or both things to compare is different, we can already
+        # tell they're not equal
+        if len(records) != len(expected_values):
+            msg = "Wrong number of records to compare: %d != %d.\n\n" % (
+                len(records),
+                len(expected_values),
+            )
+            self.fail(msg + _format_message(records, expected_values))
+
+        for index, record in enumerate(records):
+            if not _compare_candidate(record, expected_values[index]):
+                msg = (
+                    "Record doesn't match expected values at index %d.\n\n"
+                    % index
+                )
+                self.fail(msg + _format_message(records, expected_values))
