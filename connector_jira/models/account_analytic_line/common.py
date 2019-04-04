d50be5bd411b8770e15b6efc4423f80fdc2fe84d
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-04-04 20:08:05 +0200

    Fix error when force_reimport called on several records

--- connector_jira/models/account_analytic_line/common.py
+++ connector_jira/models/account_analytic_line/common.py
@@ -84,7 +84,7 @@ class JiraAccountAnalyticLine(models.Model):
 
     @api.multi
     def force_reimport(self):
-        for binding in self.jira_bind_ids:
+        for binding in self.mapped('jira_bind_ids'):
             binding.with_delay(priority=8).import_record(
                 binding.backend_id,
                 binding.jira_issue_id,
