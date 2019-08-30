f4ba6738d629e8435a3aa88e87dcb4b161784f8b
Alexey Pelykh <alexey.pelykh@gmail.com>
2019-08-30 11:25:17 +0200

    [ADD] connector_jira_tempo_base, connector_jira_tempo_project_role

--- connector_jira_tempo/models/account_analytic_line/common.py
+++ connector_jira_tempo/models/account_analytic_line/common.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Camptocamp SA
+# Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -21,42 +22,36 @@ class AccountAnalyticLine(models.Model):
 class WorklogAdapter(Component):
     _inherit = 'jira.worklog.adapter'
 
-    # SPECS
-    # http://developer.tempo.io/doc/timesheets/api/rest/latest
-    # the base path must be overridden otherwise
-    # `_get_url` will give us back a bad path including the std JIRA one
-    _tempo_api_path_base = '{server}/rest/tempo-timesheets/3/{path}'
-
-    def _tempo_get_url(self, path):
-        return self.client._get_url(path, base=self._tempo_api_path_base)
-
     def read(self, issue_id, worklog_id):
         worklog = super().read(issue_id, worklog_id)
-        if self.env.context.get('jira_worklog_no_tempo_data'):
+        if self.env.context.get(
+                'jira_worklog_no_tempo_timesheets_approval_data'):
             return worklog
         with self.handle_404():
-            worklog['_timesheet'] = self.tempo_read_approval(worklog)
+            worklog['_tempo_timesheets_approval'] = \
+                self.tempo_timesheets_approval_read(worklog)
         return worklog
 
     # This one seems useless ATM.
     # def tempo_read_worklog(self, worklog):
-    #     url = self._tempo_get_url('worklogs')
+    #     url = self._tempo_timesheets_get_url('worklogs')
     #     r = self.client._session.get(url, params={'dateFrom': '2018-01-01'})
     #     return {}
 
-    def tempo_read_approval(self, worklog):
-        username = worklog['author']['name']
-        url = self._tempo_get_url('timesheet-approval/current')
+    def tempo_timesheets_approval_read(self, worklog):
+        url = self._tempo_timesheets_get_url('timesheet-approval/current')
         with self.handle_404():
-            resp = self.client._session.get(url, params={'username': username})
-        return resp.json()
+            response = self.client._session.get(url, params={
+                'username': worklog['author']['name'],
+            })
+        return response.json()
 
-    def tempo_read_status_by_team(self, team_id, period_start):
-        url = self._tempo_get_url('timesheet-approval')
-        params = {
-            'teamId': team_id,
-            'periodStartDate': period_start
-        }
+    def tempo_timesheets_approval_read_status_by_team(
+            self, team_id, period_start):
+        url = self._tempo_timesheets_get_url('timesheet-approval')
         with self.handle_404():
-            resp = self.client._session.get(url, params=params)
-        return resp.json()
+            response = self.client._session.get(url, params={
+                'teamId': team_id,
+                'periodStartDate': period_start,
+            })
+        return response.json()
