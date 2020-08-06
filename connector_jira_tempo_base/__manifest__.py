6873261b999a4164b0b3662b30002219dd0bffb4
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-08-06 11:56:28 +0200

    [IMP] connector_jira_tempo_base: black, isort, prettier

--- connector_jira_tempo_base/__manifest__.py
+++ connector_jira_tempo_base/__manifest__.py
@@ -3,22 +3,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'JIRA Connector Tempo (base)',
-    'version': '12.0.1.1.0',
-    'category': 'Connector',
-    'website': 'https://github.com/OCA/connector-jira',
-    'author':
-        'Brainbean Apps, '
-        'Camptocamp, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Base for JIRA Connector Tempo',
-    'depends': [
-        'connector_jira',
-    ],
-    'data': [
-        'views/jira_backend.xml',
-    ],
+    "name": "JIRA Connector Tempo (base)",
+    "version": "12.0.1.1.0",
+    "category": "Connector",
+    "website": "https://github.com/OCA/connector-jira",
+    "author": "Brainbean Apps, " "Camptocamp, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Base for JIRA Connector Tempo",
+    "depends": ["connector_jira",],
+    "data": ["views/jira_backend.xml",],
 }
