634656ba05a1438bfe1363ad37d92e3f18204b01
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-08-06 12:01:07 +0200

    [IMP] connector_jira_tempo: black, isort, prettier

--- connector_jira_tempo/__manifest__.py
+++ connector_jira_tempo/__manifest__.py
@@ -1,22 +1,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'JIRA Connector Tempo',
-    'version': '12.0.2.1.1',
-    'author':
-        'Camptocamp, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'category': 'Connector',
-    'depends': [
-        'connector_jira_tempo_base',
-        'hr_timesheet',
+    "name": "JIRA Connector Tempo",
+    "version": "12.0.2.1.1",
+    "author": "Camptocamp, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Connector",
+    "depends": ["connector_jira_tempo_base", "hr_timesheet",],
+    "website": "https://github.com/OCA/connector-jira",
+    "data": [
+        "data/cron.xml",
+        "views/jira_backend_view.xml",
+        "views/timesheet_account_analytic_line.xml",
     ],
-    'website': 'https://github.com/OCA/connector-jira',
-    'data': [
-        'data/cron.xml',
-        'views/jira_backend_view.xml',
-        'views/timesheet_account_analytic_line.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
--- connector_jira_tempo/models/account_analytic_line/common.py
+++ connector_jira_tempo/models/account_analytic_line/common.py
@@ -3,33 +3,34 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
+
 from odoo.addons.component.core import Component
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     jira_tempo_status = fields.Selection(
         selection=[
-            ('approved', 'Approved'),
-            ('waiting_for_approval', 'Waiting for approval'),
-            ('ready_to_submit', 'Ready to submit'),
-            ('open', 'Open'),
+            ("approved", "Approved"),
+            ("waiting_for_approval", "Waiting for approval"),
+            ("ready_to_submit", "Ready to submit"),
+            ("open", "Open"),
         ]
     )
 
 
 class WorklogAdapter(Component):
-    _inherit = 'jira.worklog.adapter'
+    _inherit = "jira.worklog.adapter"
 
     def read(self, issue_id, worklog_id):
         worklog = super().read(issue_id, worklog_id)
-        if self.env.context.get(
-                'jira_worklog_no_tempo_timesheets_approval_data'):
+        if self.env.context.get("jira_worklog_no_tempo_timesheets_approval_data"):
             return worklog
         with self.handle_404():
-            worklog['_tempo_timesheets_approval'] = \
-                self.tempo_timesheets_approval_read(worklog)
+            worklog["_tempo_timesheets_approval"] = self.tempo_timesheets_approval_read(
+                worklog
+            )
         return worklog
 
     # This one seems useless ATM.
@@ -39,19 +40,17 @@ class WorklogAdapter(Component):
     #     return {}
 
     def tempo_timesheets_approval_read(self, worklog):
-        url = self._tempo_timesheets_get_url('timesheet-approval/current')
+        url = self._tempo_timesheets_get_url("timesheet-approval/current")
         with self.handle_404():
-            response = self.client._session.get(url, params={
-                'username': worklog['author']['name'],
-            })
+            response = self.client._session.get(
+                url, params={"username": worklog["author"]["name"],}
+            )
         return response.json()
 
-    def tempo_timesheets_approval_read_status_by_team(
-            self, team_id, period_start):
-        url = self._tempo_timesheets_get_url('timesheet-approval')
+    def tempo_timesheets_approval_read_status_by_team(self, team_id, period_start):
+        url = self._tempo_timesheets_get_url("timesheet-approval")
         with self.handle_404():
-            response = self.client._session.get(url, params={
-                'teamId': team_id,
-                'periodStartDate': period_start,
-            })
+            response = self.client._session.get(
+                url, params={"teamId": team_id, "periodStartDate": period_start,}
+            )
         return response.json()
