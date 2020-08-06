b9a5cc847cecde80e572d6c271e2601ae8e8bb72
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-08-06 11:57:48 +0200

    [MIG] connector_jira_tempo_base: Migration to 13.0

--- connector_jira_tempo_base/__manifest__.py
+++ connector_jira_tempo_base/__manifest__.py
@@ -4,14 +4,14 @@
 
 {
     "name": "JIRA Connector Tempo (base)",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "category": "Connector",
     "website": "https://github.com/OCA/connector-jira",
-    "author": "Brainbean Apps, " "Camptocamp, " "Odoo Community Association (OCA)",
+    "author": "Brainbean Apps, Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "application": False,
     "summary": "Base for JIRA Connector Tempo",
-    "depends": ["connector_jira",],
-    "data": ["views/jira_backend.xml",],
+    "depends": ["connector_jira"],
+    "data": ["views/jira_backend.xml"],
 }
