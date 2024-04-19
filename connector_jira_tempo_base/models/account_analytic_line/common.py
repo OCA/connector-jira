60807e6084a060ce9697770e7bf1ac41dd1004dd
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-04-19 08:15:46 +0200

    fixup! connector_jira_tempo_base: use web-api/webservice

--- connector_jira_tempo_base/models/account_analytic_line/common.py
+++ connector_jira_tempo_base/models/account_analytic_line/common.py
@@ -1,6 +1,9 @@
 # Copyright 2018 Camptocamp SA
 # Copyright 2019 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import json
+
+from odoo import _, exceptions
 
 from odoo.addons.component.core import Component
 
@@ -10,22 +13,42 @@ class WorklogAdapter(Component):
 
     _tempo_timesheets_api_path_base = "{server}/rest/tempo-timesheets/3/{path}"
 
-    def _tempo_timesheets_get_url(self, path):
-        return self.client._get_url(
-            path,
-            base=self._tempo_timesheets_api_path_base,
-        )
+    def _tempo_timesheets_get_webservice(self):
+        backend = self.collection.tempo_ws_backend_id
+        return backend
 
     def read(self, issue_id, worklog_id):
         worklog = super().read(issue_id, worklog_id)
         if self.env.context.get("jira_worklog_no_tempo_timesheets_data"):
             return worklog
         with self.handle_404():
-            worklog["_tempo_timesheets"] = self.tempo_timesheets_read(worklog_id)
+            tempo_timesheet = self.tempo_timesheets_read(worklog_id)
+            worklog.update(
+                {
+                    "_tempo_timesheets": tempo_timesheet,
+                    "author": tempo_timesheet["author"],
+                    "comment": tempo_timesheet["description"],
+                    "tempo_worklog_id": tempo_timesheet["tempoWorklogId"],
+                }
+            )
         return worklog
 
-    def tempo_timesheets_read(self, worklog_id):
-        url = self._tempo_timesheets_get_url("worklogs/%s" % worklog_id)
-        with self.handle_404():
-            response = self.client._session.get(url)
-        return response.json()
+    def _tempo_worklog_id(self, jira_worklog_id):
+        jira_worklog_id = int(jira_worklog_id)
+        backend = self._tempo_timesheets_get_webservice()
+        endpoint = "worklogs/jira-to-tempo"
+        payload = {"jiraWorklogIds": [jira_worklog_id]}
+        response = backend.call("post", url_params={"endpoint": endpoint}, json=payload)
+        response = json.loads(response)
+        result = response["results"]
+        for row in result:
+            if row["jiraWorklogId"] == jira_worklog_id:
+                return row["tempoWorklogId"]
+        raise exceptions.UserError(_("Could not find a matching record"))
+
+    def tempo_timesheets_read(self, jira_worklog_id):
+        tempo_worklog_id = self._tempo_worklog_id(jira_worklog_id)
+        backend = self._tempo_timesheets_get_webservice()
+        endpoint = f"worklogs/{tempo_worklog_id}"
+        response = backend.call("get", url_params={"endpoint": endpoint})
+        return json.loads(response)
