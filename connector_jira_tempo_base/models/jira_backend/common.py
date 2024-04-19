8166a1248d102bf2a04cbee83d3339ecfd0cfb44
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2024-04-19 08:15:46 +0200

    fixup! connector_jira_tempo_base: use web-api/webservice

--- connector_jira_tempo_base/models/jira_backend/common.py
+++ connector_jira_tempo_base/models/jira_backend/common.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class JiraBackend(models.Model):
     _inherit = "jira.backend"
 
-    webservice_backend_id = fields.Many2one("webservice.backend")
+    tempo_ws_backend_id = fields.Many2one("webservice.backend")
